0ea25bdfd2d962a87d2fa3c854bfdd2b11fc0029
Yasir-Hossain-Katib <85574228+Yasir-Hossain-Katib@users.noreply.github.com>
2024-02-26 11:26:48 +0600

    Create aws_resource_tracker.sh

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#############
+#Author : Katib
+#date: 25 February
+#version:v1
+##
+#This script will report the AWS resource usage
+##################
+set -x
+
+#AWS S3
+#AWS EC2
+#AWS Lambda
+#AWS IAM users
+
+#list s3 buckets
+echo "print list of s3 buckets"
+aws s3 ls > resource_tracker.txt
+
+#list EC2 instances
+echo "print list of ec2 instances"
+
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' > resource_tracker
+
+#list lambda
+echo "print list of lambda functions"
+
+
+#list IAM users
+echo "print list of IAM users"
+aws iam list-users > resource_tracker
+                        
